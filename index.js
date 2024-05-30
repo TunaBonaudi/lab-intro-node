ea6591b46385312e5148e8a290c93a0f8f14c907
Joaquin Bonaudi <jtbmelgar@gmail.com>
2024-05-30 12:41:47 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let index = this.items.findIndex(currentItem => currentItem > item);
+    if (index === -1) {
+      this.items.push(item)
+    }
+    else {
+      this.items.splice(index, 0, item);
+    }
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= 0 && pos < this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() { 
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.items.length - 1];
+  }
 
-  min() {}
+  min() { 
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() { 
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  }
+  
 
-  avg() {}
+  avg() { 
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
